518216a2f342f0bfcfe89294d550a112792f2273
svsjo <weis_jonas@mail.de>
2021-12-27 23:40:02 +0100

    a

--- UI/UI.cpp
+++ UI/UI.cpp
@@ -10,10 +10,28 @@
 class UI
 {
     private:
-        Tile field [22][12];
+        Tile * field [22][12];
+        void init()
+        {
+            for(int k = 0; k < 12; k++)
+            {   // Oben und Unten Rand
+                field[0][k] = new Tile(Color::black);
+                field[21][k] = new Tile(Color::black);
+            }
+            for(int h = 1; h < 21; h++)
+            {   // Links und Rechts Rand
+                field[h][0] = new Tile(Color::black);
+                field[h][11] = new Tile(Color::black);
+            }
+        }
 
     public:
         void draw();
+        UI()
+        {
+            clearScreen();
+            init();
+        }
 };
 
 int ji;
@@ -23,8 +41,8 @@ void UI::draw()
     hideCursor();
     clearLine();
     for(int i=0; i<sizeof(field)/sizeof(field[0]);i++)
-    {          
-        for(int j=0; j<sizeof(field[i])/sizeof(Tile);j++)
+    {        
+        for(int j=0; j<sizeof(field[i])/sizeof(Tile *);j++)
         {                  
             if(j==ji)
             {
@@ -35,7 +53,7 @@ void UI::draw()
             }                
             moveTo(i,j);
             moveUp(1);     
-            puts("■");          
+            puts("█");      
         }  
      
     } 
