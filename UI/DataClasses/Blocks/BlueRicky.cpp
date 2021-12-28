ffdbcdc45569082ffeca49bd3d527c8cf8ed7654
Luca <i20024@hb.dhbw-stuttgart.de>
2021-12-28 13:08:24 +0100

    done BlueRicky

--- UI/DataClasses/Blocks/BlueRicky.cpp
+++ UI/DataClasses/Blocks/BlueRicky.cpp
@@ -14,42 +14,84 @@ class BlueRicky:TetrisBlock
                 /*    X    
                       A
                     X X  */
-                matrix[0][1] = matrix[0][0];
-                matrix[1][1] = matrix[0][1];
-                matrix[2][1] = matrix[0][2];
-                matrix[2][0] = matrix[1][2];
+                matrix[0][1] = matrix[1][0];
+                matrix[1][1] = matrix[1][1]; //stays same
+                matrix[2][1] = matrix[1][2];
+                matrix[2][0] = matrix[2][2];
 
                 // Zurücksetzen der nun leeren Felder
-                matrix[0][0] = nullptr;
-                matrix[0][2] = nullptr;
+                matrix[1][0] = nullptr;
                 matrix[1][2] = nullptr;
+                matrix[2][2] = nullptr;
 
                 anchorPositionX = 1; // Spalte 1
                 anchorPositionY = 1; // Reihe 1
                 angle = 90;
                 break;
+
             case 90:
-                /* code */
+                /*  X    
+                    X A X
+                      */
+                matrix[0][0] = matrix[2][0];
+                matrix[1][0] = matrix[2][1];
+                matrix[1][1] = matrix[1][1]; //stays same
+                matrix[1][2] = matrix[0][1];
+
+                // Zurücksetzen der nun leeren Felder
+                matrix[2][0] = nullptr;
+                matrix[2][1] = nullptr;
+                matrix[0][1] = nullptr;
+
+                angle = 180;
                 break;
+
             case 180:
-                /* code */
+                 /*    X X 
+                       A 
+                       X     */
+                matrix[0][2] = matrix[0][0];
+                matrix[0][1] = matrix[1][0];
+                matrix[1][1] = matrix[1][1]; //stays same
+                matrix[2][1] = matrix[1][2];
+
+                matrix[0][0] = nullptr;
+                matrix[1][0] = nullptr;
+                matrix[1][2] = nullptr;
+
+                angle = 270;
                 break;
             case 270:
-                /* code */
+                 /*     
+                    X  A  X
+                          X   */
+
+                matrix[2][2] = matrix[0][2];
+                matrix[1][2] = matrix[0][1];
+                matrix[1][1] = matrix[1][1]; //stays same
+                matrix[1][0] = matrix[2][1];
+
+                matrix[0][2] = nullptr;
+                matrix[0][1] = nullptr;
+                matrix[2][1] = nullptr;
+
+                angle = 0;
                 break;
-            default:
+                
+                default:
                 break;
             }
         }
         
         BlueRicky()
-        {
-            /*  X A X    
+        {                       
+            /*
+                X A X       
                     X  */
-            matrix[0][0] = new Tile(Color::orange);
-            matrix[0][1] = new Tile(Color::orange);
-            matrix[0][2] = new Tile(Color::orange);
+            matrix[1][0] = new Tile(Color::orange);
+            matrix[1][1] = new Tile(Color::orange);
             matrix[1][2] = new Tile(Color::orange);
+            matrix[2][2] = new Tile(Color::orange);
             anchorPositionX = 1; // Spalte 1
             anchorPositionY = 0; // Reihe 0
             angle = 0;
