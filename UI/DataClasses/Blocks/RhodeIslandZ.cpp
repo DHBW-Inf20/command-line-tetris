57cee4b58dc09a5606168396dc6f3f1160bb01ac
svsjo <weis_jonas@mail.de>
2021-12-29 13:43:38 +0100

    Added RhodeZ rotation

--- UI/DataClasses/Blocks/RhodeIslandZ.cpp
+++ UI/DataClasses/Blocks/RhodeIslandZ.cpp
@@ -8,7 +8,44 @@ class RhodeIslandZ:public TetrisBlock
     public:
         void rotateRight() override
         {
+            switch (angle)
+            {
+            case 0:
+            /* case 180: */
+                /*    X    
+                      A X
+                        X   */
+                matrix[0][1] = matrix[1][0];
+                matrix[1][2] = matrix[0][1];
+                matrix[1][1] = matrix[1][1]; // same
+                matrix[2][2] = matrix[0][2];
+                angle = 90;
+
+                // Zurücksetzen der nicht mehr genutzten Felder
+                matrix[1][0] = nullptr;
+                matrix[0][2] = nullptr;
+
+                break;
             
+            case 90:
+            /* case 270: */
+                /*    X X     
+                    X A    */
+                matrix[0][1] = matrix[1][2];
+                matrix[0][2] = matrix[2][2];
+                matrix[1][1] = matrix[1][1]; // same
+                matrix[1][0] = matrix[0][1];
+                angle = 0;
+
+                // Zurücksetzen der nicht mehr genutzten Felder
+                matrix[1][2] = nullptr;
+                matrix[2][2] = nullptr;
+
+                break;
+
+            default:
+                break;
+            }
         }
         RhodeIslandZ()
         {
